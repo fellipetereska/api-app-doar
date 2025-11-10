86e93ed30645161b6a04785071540ce3cf81ccb1
fellipetereska <fellipetereska@gmail.com>
2025-11-10 09:56:34 -0300

    att pwd

--- Api.AppDoar/PersistenciaDB/ConnectionDB.cs
+++ Api.AppDoar/PersistenciaDB/ConnectionDB.cs
@@ -4,7 +4,7 @@ namespace Api.AppDoar.PersistenciaDB
 {
     public static class ConnectionDB
     {
-        private static readonly string connectionString = "Server=193.203.175.155;Database=u271392345_db_doar;Uid=u271392345_admin_doar;Pwd=fte3009Aa.;";
+        private static readonly string connectionString = "Server=193.203.175.155;Database=u271392345_db_doar;Uid=u271392345_admin_doar;Pwd=fte3009@Aa.;";
         // private static readonly string connectionString = "Server=localhost;Database=db_doar;Uid=u271392345_admin_doar;Pwd=200518;";
         public static MySqlConnection GetConnection()
         {
